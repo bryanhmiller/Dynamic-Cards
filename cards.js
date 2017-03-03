7cc9cef818b92e589032b9f71564afcd78fabf74
bryanhmiller <bryankalos@hotmail.com>
2017-03-02 18:50:42 -0600

    Put basic elements of the assignment in index.html, added button and listener to cards.js, added card style to cards.css

--- cards.js
+++ cards.js
@@ -1,1 +1,12 @@
 console.log("I am here.");
+// When the user enters in text into the text area and then clicks the create button, 
+// create a new card element in the DOM. You decide the height/width of the card.
+var createCardButton = document.getElementById("create-button");
+createCardButton.addEventListener("click", createCard);
+
+function createCard() {
+	console.log("Button Works!");
+}
+
+// When the user clicks the Delete button, the containing card, and no other cards, should 
+// then be removed from the DOM. Not just made invisible, actually removed from the DOM.
