d82391d79a10d8b591aef5b15cb418beff3ff59c
bryanhmiller <bryankalos@hotmail.com>
2017-03-04 13:56:44 -0600

    Found the path to delete cards, and now the cards delete like they are supposed to.

--- cards.js
+++ cards.js
@@ -5,6 +5,7 @@ var cardHolderDiv = document.getElementById("card-holder");
 var cardText = []
 var cardInput = "";
 var i = 0;
+var unpinCard = document.getElementById("card-holder");
 // var cardOutput = document.getElementById('card-output');
 
 function captureInput() {
@@ -16,24 +17,37 @@ function captureInput() {
 	console.log("cardText", cardText); 
 	createCard();
 }
-	
 
 cardHolderDiv.innerHTML = "";
 function createCard(cardInput) {
 	var newCard = "";
 	newCard += `<div class="card-parent" id="cardBox-${i}">`;
-	newCard += `<div class="cards">  ${cardText[i]} </div>`;
-	newCard += `</div>`
+	newCard += `<div class="pin-holder">`;
+	newCard += `<img class="pin" src="http://cliparts.co/cliparts/gTe/5GX/gTe5GXRKc.jpg">`;
+	newCard += `</div>`;
+	newCard += `<div class="cards">  ${cardText[i]}`;
+	newCard += `</div>`;
+	newCard += `</div>`;
 	cardHolderDiv.innerHTML += newCard;
 	i++; 
-	
 }
 
-
+function deleteCard(event) {
+	if(event.target.className === "pin") {
+		for (var q=0; q<event.path.length; q++) {
+			if(event.path[q].className === "card-parent") {
+				var cardToDelete = event.path[q];
+				console.log("cardToDelete", cardToDelete);
+				unpinCard.removeChild(cardToDelete);
+			}
+		}
+	}	
+}
 // When the user clicks the Delete button, the containing card, and no other cards, should 
 // then be removed from the DOM. Not just made invisible, actually removed from the DOM.
 
 createCardButton.addEventListener("click", getThisPartyStarted);
+unpinCard.addEventListener("click", deleteCard);
 
 function getThisPartyStarted() {
 	captureInput();
